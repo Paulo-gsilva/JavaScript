3275fd791a38aa6b37fc391798c48b98f4d62a20
Paulo-gsilva <guigui041201@gmail.com>
2022-03-03 23:03:00 -0300

    adicionando webpack

---
+++ modelo webpack/src/index.js
@@ -0,0 +1,9 @@
+class Pessoa{
+    constructor(nome, sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
+
+const joao = new Pessoa('João', 'Victor');
+console.log(joao);
