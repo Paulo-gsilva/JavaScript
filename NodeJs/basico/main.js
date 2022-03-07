ee102b5faf8775a8ac7b20d904c4e69f159c3147
Paulo-gsilva <guigui041201@gmail.com>
2022-03-07 10:22:17 -0300

    conceitos de js - exports e require

---
+++ NodeJs/basico/main.js
@@ -0,0 +1,4 @@
+const modulo = require('./modulo');
+//const surname = require('./modulo').surname; //importa apenas uma chave
+//const {name, surname, fullName} = require('./modulo');
+console.log(modulo);
---
+++ NodeJs/basico/modulo.js
@@ -0,0 +1,15 @@
+const name = 'Pedro';
+const surname = 'Pereira';
+
+const fullName = () => `${name} ${surname}`
+const count = () => 1 +1;
+//exportando
+//exports.
+//module.exports.name = name;
+//module.exports.surname = surname;
+//module.exports.fullName = fullName; 
+module.exports = {
+    name, surname, fullName
+}
+
+console.log(module.exports);
