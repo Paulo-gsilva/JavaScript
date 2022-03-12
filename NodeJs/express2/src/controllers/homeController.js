40730486d33e48ac07aa114cd1a81ca1d678990e
Paulo-gsilva <guigui041201@gmail.com>
2022-03-12 16:51:50 -0300

    Criando dados e exportando para o mongoDB

--- NodeJs/express2/src/controllers/homeController.js
+++ NodeJs/express2/src/controllers/homeController.js
@@ -1,3 +1,13 @@
+/*
+ISSO SERÁ FEITO DE OUTRA FORMA
+const HomeModel = require('../models/HomeModel'); //chamando o model do banco de dados
+HomeModel.create({ //retorna uma promise com dados contidos em um documento
+    id: 234567,
+    nome: 'paulinho tuntun'
+})
+    .then(dados => console.log(dados))
+    .catch(e => console.log(e));
+*/
 exports.paginaIncial = (req, res) => {
     res.render('index');
     return;
