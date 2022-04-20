c2e34a7bbda0564e731e3eb4f5ea79cbc28843cd
Paulo-gsilva <guigui041201@gmail.com>
2022-04-19 21:18:54 -0300

    useCallBack utilizado para armazenar funções e componentes que não sofrem alterações durante o funcionamento, usá-lo garante que o componente não seja recriado e gaste mais memória

--- React/reacthooks/src/App.js
+++ React/reacthooks/src/App.js
@@ -2,20 +2,20 @@ import React, { useCallback, useState } from 'react';
 import './App.css';
 
 //react.memo verifica se isso mudou ou não
-const Button = React.memo(({ incrementButton }) => {
+const Button = ({ incrementButton }) => {
   console.log('Filho renderizado');
   return <button onClick={() => incrementButton(10)}>C1</button>
-});
+};
 
 function App() {
   const [counter, setCounter] = useState(0);
 
   //toda vez que a página é renderizada essa função é criada novamente
-  //em funções mais complexas é recomendado o uso de cuseCallBack
+  //em funções mais complexas é recomendado o uso de useCallBack
   //não tem sentido renderizar novamente algo que não muda
 
   //useCallBack salva a função, apenas será recriado se o dependente alterar []
-  const incrementButton = useCallback((num) => {
+  const incrementCounter = useCallback((num) => {
     setCounter((counter) => counter + num); //callback
   }, []);
 
@@ -28,7 +28,7 @@ function App() {
   return (
     <div className="App">
       <h1>C1: {counter}</h1>
-      <Button incrementButton={incrementButton} />
+      <Button incrementButton={incrementCounter} />
     </div >
   );
 }
