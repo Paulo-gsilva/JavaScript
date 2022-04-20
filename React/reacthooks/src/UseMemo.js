7454142aa44f0c364d140d4bae01a786c4885bac
Paulo-gsilva <guigui041201@gmail.com>
2022-04-19 22:30:45 -0300

    useMemo é usado para memorizar componentes e valores

--- React/reacthooks/src/UseMemo.js
+++ React/reacthooks/src/UseMemo.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
 const Post = ({ post }) => {
@@ -12,6 +12,7 @@ const Post = ({ post }) => {
 
 function UseMemoApp() {
     const [posts, setPosts] = useState([]);
+    const [value, setValue] = useState('');
 
     useEffect(() => { //assincrono
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -24,8 +25,20 @@ function UseMemoApp() {
     //a primeira renderização ocorre para renderizar os componentes, a segunda ocorre quando os dados da API são carregados e entram nos atributos
     return (
         <div className='App'>
-            <h1>UseMemo</h1>
-            {posts.length > 0 && posts.map((post) => <Post post={post} key={post.id} />)}
+            <p>
+                {/*toda vez que escrevo algo no input o estado de value é alterado, isso faz com que todos os componentes sejam renderizados novamente*/}
+                {/*os posts também são renderizados novamente, isso pode ocasionar lentidão*/}
+                <input type='search' value={value} onChange={(e) => setValue(e.target.value)} />
+            </p>
+            {/*useMemo é ultilizado para memorizar componentes ou valores*/}
+            {useMemo(() => {
+                return (
+                    posts.length > 0 &&
+                    posts.map((post) => {
+                        return <Post post={post} key={post.id} />;
+                    })
+                );
+            }, [posts])}
             {posts.length <= 0 && (
                 <p>Sem posts no momento</p>
             )}
