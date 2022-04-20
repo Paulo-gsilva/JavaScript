3ece1aeb77136b5df61dc205f801e293a2c10519
Paulo-gsilva <guigui041201@gmail.com>
2022-04-19 23:05:46 -0300

    useRef é usado para 'conectar' valores

--- React/reacthooks/src/UseMemo.js
+++ React/reacthooks/src/UseMemo.js
@@ -1,11 +1,11 @@
 import P from 'prop-types';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 
-const Post = ({ post }) => {
+const Post = ({ post, handleClick }) => {
     return (
         <div key={post.id} className='post'>
-            <h1>{post.title}</h1>
+            <h1 onClick={() => handleClick(post.title)}>{post.title}</h1>
             <p>{post.body}</p>
         </div>
     );
@@ -16,12 +16,23 @@ Post.propTypes = {
         id: P.number,
         title: P.string,
         body: P.string
-    })
+    }),
+    onClick: P.func
 }
 
 function UseMemoApp() {
     const [posts, setPosts] = useState([]);
     const [value, setValue] = useState('');
+    const input = useRef(null);
+
+    const handleClick = (value) => {
+        setValue(value);
+    }
+
+    useEffect(() => { //sempre que o value for alterado o input ficará em foco
+        input.current.focus();
+        console.log(input.current);
+    }, [value])
 
     useEffect(() => { //assincrono
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -37,14 +48,15 @@ function UseMemoApp() {
             <p>
                 {/*toda vez que escrevo algo no input o estado de value é alterado, isso faz com que todos os componentes sejam renderizados novamente*/}
                 {/*os posts também são renderizados novamente, isso pode ocasionar lentidão*/}
-                <input type='search' value={value} onChange={(e) => setValue(e.target.value)} />
+                <input type='search' ref={input} value={value} onChange={(e) => setValue(e.target.value)} />
+                {/*ref é usado para conectar o dono do atributo ao parametro do ref*/}
             </p>
             {/*useMemo é ultilizado para memorizar componentes ou valores*/}
             {useMemo(() => {
                 return (
                     posts.length > 0 &&
                     posts.map((post) => {
-                        return <Post post={post} key={post.id} />;
+                        return <Post post={post} handleClick={handleClick} key={post.id} />;
                     })
                 );
             }, [posts])}
