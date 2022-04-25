633381473b7ea064f00b477d50cdbca3d76e467a
guigui041201@gmail.com <guigui041201@gmail.com>
2022-04-25 09:11:23 -0300

    UseContext é usado para atribuir valores de forma global, por meio de componentes, sem a necessidade de usar inifitas props dentro de um componente

--- React/reacthooks/src/UseContext.js
+++ React/reacthooks/src/UseContext.js
@@ -1,12 +1,44 @@
-import P from 'prop-types';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { createContext, useContext } from 'react';
 import './App.css';
+
+const globalState = {
+    title: 'titulo do contexto',
+    subtitle: 'subtitulo do contexto',
+    counter: 0
+}
+
+const GlobalContext = createContext();
+
+const Div = () => {
+    return (
+        <H1 />
+    );
+}
+
+const H1 = () => {
+    const context = useContext(GlobalContext); //H1 irá capturar o valor passado para o GlobalState
+    return (
+        <>
+            <h1>{context.title}</h1>
+            <P />
+        </>
+    );
+}
+
+const P = () => {
+    const context = useContext(GlobalContext);
+    return (
+        <p>{context.subtitle}</p>
+    );
+}
+
 
 function UseContextApp() {
     return (
-        <div className='App'>
-            <h1>Banana</h1>
-        </div>
+        <GlobalContext.Provider value={globalState}> {/*todos os componentes que estiverem dentro do GlobalContext irão receber o globalstate*/}
+            {/*alterar um estado do globalstate irá refletir em todos os descendentes*/}
+            <Div />
+        </GlobalContext.Provider>
     );
 }
 
