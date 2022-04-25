a5de35eb3861bd9c10f39893bc4893df7a677074
guigui041201@gmail.com <guigui041201@gmail.com>
2022-04-25 08:01:20 -0300

    iniciando useContext

---
+++ React/reacthooks/src/UseContext.js
@@ -0,0 +1,13 @@
+import P from 'prop-types';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import './App.css';
+
+function UseContextApp() {
+    return (
+        <div className='App'>
+            <h1>Banana</h1>
+        </div>
+    );
+}
+
+export default UseContextApp;
--- React/reacthooks/src/index.js
+++ React/reacthooks/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import UseMemoApp from './UseMemo';
+import UseContextApp from './UseContext';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UseMemoApp />
+    <UseContextApp />
   </React.StrictMode>
 );
 
